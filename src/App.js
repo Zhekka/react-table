b1da38798ba2ee6e0cdf12aae2ef3b08959b3c03
eugene <kirnasuk1284@gmail.com>
2021-03-19 19:59:01 +0200

    first test react table

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import Table from "./Table/Table";
+import React, {useMemo} from "react";
+
+function App() {
+    const columns = [
+        {
+            Header: "TV Show",
+            columns: [
+                {
+                    Header: "Name",
+                    accessor: "show.name"
+                },
+                {
+                    Header: "Type",
+                    accessor: "show.type"
+                }
+            ]
+        },
+        {
+            Header: "Details",
+            columns: [
+                {
+                    Header: "Language",
+                    accessor: "show.language"
+                },
+                {
+                    Header: "Genre(s)",
+                    accessor: "show.genres"
+                },
+                {
+                    Header: "Runtime",
+                    accessor: "show.runtime"
+                },
+                {
+                    Header: "Status",
+                    accessor: "show.status"
+                }
+            ]
+        }
+    ];
+
+
+    const data = [
+        {
+            "show": {
+                "name": "The Snow Spider",
+                "type": "Scripted",
+                "language": "English",
+                "genres": [
+                    "Drama",
+                    "Fantasy"
+                ],
+                "status": "In Development",
+                "runtime": 30,
+            }
+        },
+        {
+            "show": {
+                "name": "The Snow Spider2",
+                "type": "Scripted2",
+                "language": "English2",
+                "genres": [
+                    "Drama2",
+                    "Fantasy2"
+                ],
+                "status": "In Development2",
+                "runtime": 31,
+            }
+        }];
+
+    return (
+        <div className="App">
+            <Table columns={columns} data={data}/>
+
+        </div>
+    );
+}
+
+export default App;
